0877aff393da0713cf091471fc0bb54f76a07ee3
HaihengDev <haihenglim9@gmail.com>
2026-03-05 00:36:24 +0700

    Starter react routes

--- project_01 (Anime Figure Shop)/client/src/App.jsx
+++ project_01 (Anime Figure Shop)/client/src/App.jsx
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
-const App = () => {};
+const App = () => {
+  return <Outlet />;
+};
 
 export default App;
--- project_01 (Anime Figure Shop)/client/src/main.jsx
+++ project_01 (Anime Figure Shop)/client/src/main.jsx
@@ -2,22 +2,23 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
 import Home from './pages/Home.jsx';
 import Cart from './pages/Cart.jsx';
+import Admin from './pages/Admin.jsx';
 
 const routes = createBrowserRouter([
   {
     path: '/',
     children: [
-      { index: true, children: <Home /> },
-      { path: '/cart', children: <Cart /> },
+      { index: true, element: <Home /> },
+      { path: '/cart', element: <Cart /> },
+      { path: '/admin', element: <Admin /> },
     ],
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 );
