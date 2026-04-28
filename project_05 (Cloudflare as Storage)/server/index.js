8b13fbe0576fc0386afac3d295e585f2db391b23
HaihengDev <haihenglim9@gmail.com>
2026-04-28 13:46:50 +0700

    Add package.json for server setup with dependencies and scripts

---
+++ project_05 (Cloudflare as Storage)/server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+const server = createServer(app);
+const PORT = process.env.PORT || 8888;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
+});
