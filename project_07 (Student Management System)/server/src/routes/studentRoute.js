7b1f76dc00a8279bce5c5f02e2a9df32cb37dd28
HaihengDev <haihenglim9@gmail.com>
2026-06-27 13:09:46 +0700

    feat: update student routes to include CRUD operations

--- project_07 (Student Management System)/server/src/routes/studentRoute.js
+++ project_07 (Student Management System)/server/src/routes/studentRoute.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { getAllStudents } from '../controllers/studentController.js';
+import {
+  getAllStudents,
+  getStudentById,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+} from '../controllers/studentController.js';
 
 const router = express.Router();
 
 router.get('/', getAllStudents);
+router.get('/:id', getStudentById);
+router.get('/', createStudent);
+router.get('/:id', updateStudent);
+router.get('/:id', deleteStudent);
 
 export default router;
