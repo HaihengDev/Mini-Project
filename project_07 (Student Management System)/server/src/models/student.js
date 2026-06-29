15f464afffcb332377704d4ca3ec085f41a4f244
HaihengDev <haihenglim9@gmail.com>
2026-06-29 17:21:48 +0700

    feat: update student schema export and add teacher model file

--- project_07 (Student Management System)/server/src/models/student.js
+++ project_07 (Student Management System)/server/src/models/student.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-export const studentSchema = new mongoose.Schema({
+const studentSchema = new mongoose.Schema({
   studentId: {
     type: String,
     required: true,
