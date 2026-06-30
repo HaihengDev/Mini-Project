d6e7e28b7f585c4d3a4a8105487312f0d59b9b68
HaihengDev <haihenglim9@gmail.com>
2026-06-30 18:18:42 +0700

    feat: add lecturer field to course schema

--- project_07 (Student Management System)/server/src/models/course.js
+++ project_07 (Student Management System)/server/src/models/course.js
@@ -18,6 +18,10 @@ const courseSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    lecturer: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true },
 );
