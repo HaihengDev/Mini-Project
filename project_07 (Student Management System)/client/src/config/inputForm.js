5efc13b315b8a6afd8c5a1d56e9f9187d60f9183
HaihengDev <haihenglim9@gmail.com>
2026-07-06 21:59:44 +0700

    feat: add teacher form; include fields for TeacherId, FirstName, LastName, Gender, Role, and Salary

--- project_07 (Student Management System)/client/src/config/inputForm.js
+++ project_07 (Student Management System)/client/src/config/inputForm.js
@@ -31,3 +31,86 @@ export const roomForm = [
     placeholder: 'Enter Room Id: ',
   },
 ];
+
+export const studentForm = [
+  {
+    id: 'student-id',
+    label: 'StudentId: ',
+    htmlFor: 'student-id',
+    type: 'text',
+    placeholder: 'Enter Student Id:',
+  },
+  {
+    id: 'student-first-name',
+    label: 'FirstName: ',
+    htmlFor: 'student-first-name',
+    type: 'text',
+    placeholder: 'Enter Student First Name: ',
+  },
+  {
+    id: 'student-last-name',
+    label: 'LastName: ',
+    htmlFor: 'student-last-name',
+    type: 'text',
+    placeholder: 'Enter Student Last Name: ',
+  },
+  {
+    id: 'student-gender',
+    label: 'Gender: ',
+    htmlFor: 'student-gender',
+    type: 'text',
+    placeholder: 'Enter Student Gender: ',
+  },
+  {
+    id: 'date-of-birth',
+    label: 'DateOfBirth: ',
+    htmlFor: 'date-of-birth',
+    type: 'date',
+    placeholder: '',
+  },
+];
+
+export const teacherForm = [
+  {
+    id: 'teacher-id',
+    label: 'TeacherId',
+    htmlFor: 'teacher-id',
+    type: 'text',
+    placeholder: 'Enter Teacher Id',
+  },
+  {
+    id: 'teacher-first-name',
+    label: 'FirstName: ',
+    htmlFor: 'teacher-first-name',
+    type: 'text',
+    placeholder: 'Enter Teacher First Name',
+  },
+  {
+    id: 'teacher-last-name',
+    label: 'LastName: ',
+    htmlFor: 'teacher-last-name',
+    type: 'text',
+    placeholder: 'Enter Teacher Last Name',
+  },
+  {
+    id: 'teacher-gender',
+    label: 'Gender: ',
+    htmlFor: 'teacher-gender',
+    type: 'text',
+    placeholder: 'Enter Teacher Gender',
+  },
+  {
+    id: 'role',
+    label: 'Role: ',
+    htmlFor: 'role',
+    type: 'text',
+    placeholder: 'Enter Role',
+  },
+  {
+    id: 'salary',
+    label: 'Salary: ',
+    htmlFor: 'salary',
+    type: 'number',
+    placeholder: 'Enter Salary',
+  },
+];
