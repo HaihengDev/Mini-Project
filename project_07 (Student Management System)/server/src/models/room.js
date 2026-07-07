6503595d25b0b7870b9cc640f95cbab64fe04c77
HaihengDev <haihenglim9@gmail.com>
2026-07-07 19:04:58 +0700

    feat: remove required constraint from teacher field in Course and Room schemas

--- project_07 (Student Management System)/server/src/models/room.js
+++ project_07 (Student Management System)/server/src/models/room.js
@@ -15,7 +15,6 @@ const roomSchema = new mongoose.Schema(
     teacher: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Teacher',
-      required: true,
     },
   },
   { timestamps: true },
