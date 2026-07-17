6a001d5780bf79ab2574cd170b4302d73713ae89
HaihengDev <haihenglim9@gmail.com>
2026-07-17 21:59:01 +0700

    feat: add student export functionality with ExcelJS
    - Added exceljs dependency to package.json for Excel file generation.
    - Implemented exportList function in studentController to export student data as an Excel file.
    - Updated studentRoute to include a new route for downloading the student list.

--- project_07 (Student Management System)/server/src/controllers/studentController.js
+++ project_07 (Student Management System)/server/src/controllers/studentController.js
@@ -1,3 +1,4 @@
+import ExcelJs from 'exceljs';
 import studentService from '../services/studentService.js';
 
 export const getAllStudents = async (req, res) => {
@@ -46,6 +47,62 @@ export const createStudent = async (req, res) => {
       message: 'Student is created successfully!',
       studentCreated,
     });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Server Error!',
+    });
+  }
+};
+
+export const exportList = async (req, res) => {
+  try {
+    const students = await studentService.getAll();
+
+    const workbook = new ExcelJs.Workbook();
+    const worksheet = workbook.addWorksheet('Students');
+
+    worksheet.columns = [
+      { header: 'StudentId', key: 'studentId', width: 10 },
+      { header: 'StudentFirstName', key: 'studentFirstName', width: 25 },
+      { header: 'StudentLastName', key: 'studentLastName', width: 35 },
+      { header: 'Gender', key: 'gender', width: 15 },
+    ];
+
+    students.forEach((student) => {
+      const row = worksheet.addRow(student);
+
+      row.font = {
+        name: 'Times New Roman',
+        size: 12,
+      };
+    });
+
+    const headerRow = worksheet.getRow(1);
+    headerRow.font = {
+      name: 'Times New Roman',
+      size: 14,
+      bold: true,
+      color: { argb: 'FFFFFF' },
+    };
+
+    headerRow.fill = {
+      type: 'pattern',
+      pattern: 'solid',
+      fgColor: { argb: '1E90FF' },
+    };
+
+    res.setHeader(
+      'Content-Type',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    );
+
+    res.setHeader(
+      'Content-Disposition',
+      'attachment; filename="student_list.xlsx"',
+    );
+
+    await workbook.xlsx.write(res);
+    res.status(200).end();
   } catch (err) {
     res.status(500).json({
       message: 'Server Error!',
--- project_07 (Student Management System)/server/src/routes/studentRoute.js
+++ project_07 (Student Management System)/server/src/routes/studentRoute.js
@@ -3,11 +3,13 @@ import {
   createStudent,
   getAllStudents,
   getStudentById,
+  exportList,
 } from '../controllers/studentController.js';
 
 const router = express.Router();
 
 router.get('/', getAllStudents);
+router.get('/download', exportList);
 router.get('/:id', getStudentById);
 router.post('/', createStudent);
 
